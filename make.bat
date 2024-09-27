4460da83f57e634eb32948dadb907800233d2379
JORGE ANDRES MARLES FLOREZ <jamarlesf@gmail.com>
2024-09-26 19:48:07 -0500

    chore: apply corrections to make.bat and README according to teacher revision

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Simula un Makefile con varias reglas en un archivo batch
 
-if "%1" == "setup" goto setup
+if "%1" == "dependencies" goto dependencies
 if "%1" == "lint" goto lint
 if "%1" == "test" goto test
 if "%1" == "run" goto run
@@ -11,39 +11,34 @@ goto end
 
 
 
-:setup
-python -m venv venv
-call venv\Scripts\activate
+:dependencies
 pip install -r requirements.txt
 goto end
 
 :lint
-call venv\Scripts\activate
 ruff format .
 goto end
 
 :test
-call venv\Scripts\activate
 pytohn manage.py test
 goto end
 
 :run
-call venv\Scripts\activate
 python manage.py runserver
 goto end
 
 :migrate
-call venv\Scripts\activate
+python manage.py makemigrations
 python manage.py migrate
 goto end
 
 :help
 echo Available Commands:
-echo setup      Create and activate the Virtual Environment, and install dependencies
-echo lint       Format python files
-echo test       Run Django tests
-echo run        Run Django server
-echo migrate    Apply modifications to Database
+echo dependencies       Install python dependencies
+echo lint               Format python files
+echo test               Run Django tests
+echo run                Run Django server
+echo migrate            Apply modifications to Database
 goto end
 
 :end
