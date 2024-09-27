097d024ce63e7a5dea63581bdb94bf7f08a479b0
JORGE ANDRES MARLES FLOREZ <jamarlesf@gmail.com>
2024-09-26 21:29:17 -0500

    feat: register Client in Django admin

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import ProductType
+from .models import ProductType, Client
 
 # Register your models here.
 @admin.register(ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'status']
     search_fields = ['name']
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'phone']
+    search_fields = ['first_name', 'last_name', 'email']
