23e25f13f4375e4b57ae0930c2c76fa7b1f7e09e
LizzieKar <lizziehelen65@gmail.com>
2024-09-26 22:15:52 -0500

    feat: register employee in django admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProductType, Client
+from .models import ProductType, Client, Employee
 
 # Register your models here.
 @admin.register(ProductType)
@@ -12,3 +12,8 @@ class ProductTypeAdmin(admin.ModelAdmin):
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'phone']
     search_fields = ['first_name', 'last_name', 'email']
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'role']
+    search_fields = ['first_name', 'last_name', 'email', 'role']
