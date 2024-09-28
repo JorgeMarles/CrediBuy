173212bff5e21d91eb726e76fb49dbbf221c88bd
JORGE ANDRES MARLES FLOREZ <jamarlesf@gmail.com>
2024-09-27 23:52:26 -0500

    chore: add payment model and migration

--- products/models.py
+++ products/models.py
@@ -63,3 +63,15 @@ class Credit(models.Model):
     status = models.CharField(max_length=30, choices=STATUSES)
     debt = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     total_payments = models.IntegerField(validators=[MinValueValidator(0)])
+
+class Payment(models.Model):
+    STATUSES = {
+        "pending": "Pending",
+        "completed": "Completed",
+        "delayed": "Delayed"
+    }
+    credit = models.ForeignKey(Credit, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    due_to = models.DateField()
+    status = models.CharField(max_length=30, choices=STATUSES)
+    value_delayed = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
