3281b0b27f0324e7cff79f0d0469bbd7f22bc774
JORGE ANDRES MARLES FLOREZ <jamarlesf@gmail.com>
2024-09-27 23:51:50 -0500

    feat: register credit in django admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProductType, Client, Employee, Administrator, Product
+from .models import ProductType, Client, Employee, Administrator, Product, Credit
 
 # Register your models here.
 @admin.register(ProductType)
@@ -27,3 +27,17 @@ class AdministratorAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'price', 'description', 'stock']
     search_fields = ['name', 'description']
+
+@admin.register(Credit)
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_client', 'get_product', 'created_at', 'status', 'debt', 'total_payments']
+    search_fields = ['get_client', 'get_product', 'status']
+    list_select_related = ['product', 'client']
+
+    @admin.display(description="Client")
+    def get_client(self, obj: Credit) -> str:
+        return obj.client.__str__()
+    
+    @admin.display(description="Product")
+    def get_product(self, obj: Credit) -> str:
+        return obj.product.__str__()
